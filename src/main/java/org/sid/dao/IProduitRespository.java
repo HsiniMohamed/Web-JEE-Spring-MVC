024aa671a3a3db8369c46378df1bbb0097d9a8b7
Mohamed Hsini <hssinimohamed5@gmail.com>
2024-01-24 13:20:11 +0100

    Couche DAO Spring Data et IOC

--- src/main/java/org/sid/dao/IProduitRespository.java
+++ src/main/java/org/sid/dao/IProduitRespository.java
@@ -1,8 +1,11 @@
 package org.sid.dao;
 
 import org.sid.entities.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IProduitRespository extends JpaRepository<Produit, Long>{
 
+	public Page<Produit> findByDesignationContains(String mc, Pageable pageable);
 }
